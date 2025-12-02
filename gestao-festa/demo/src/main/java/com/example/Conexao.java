369e7c40401c3bf67139c71150dafdfae2c9e96f
farolfa <raulgoncalves.profissional@gmail.com>
2025-12-02 18:44:43 +0000

    test1

---
+++ gestao-festa/demo/src/main/java/com/example/Conexao.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Conexao {
+    public void conectar(){
+        String url="sqlserver://localhost:";
+        String user;
+        String password;
+        Connection conn = DriverManager();
+    }
+}
